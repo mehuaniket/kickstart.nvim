ca5fbe39742b1f4c5aee5b165722445eaaf6c507
Aniket Patel <8078990+mehuaniket@users.noreply.github.com>
2024-09-23 11:32:06 +0530

    Added obsidian and which-key path

--- init.lua
+++ init.lua
@@ -320,6 +320,7 @@ require('lazy').setup({
       spec = {
         { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
         { '<leader>d', group = '[D]ocument' },
+        { '<leader>o', group = '[O]bsidian' },
         { '<leader>r', group = '[R]ename' },
         { '<leader>s', group = '[S]earch' },
         { '<leader>w', group = '[W]orkspace' },
