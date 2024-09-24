3f650b4e9489571856f735b350234d13c9adfeb7
Aniket Patel <8078990+mehuaniket@users.noreply.github.com>
2024-09-24 19:27:32 +0530

    Added neoggit and lualine plugin

--- init.lua
+++ init.lua
@@ -168,6 +168,7 @@ vim.opt.scrolloff = 10
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+vim.api.nvim_set_keymap('i', 'jj', '<Esc>', { noremap = false })
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
---
+++ lua/custom/plugins/lualine.lua
@@ -0,0 +1,46 @@
+return {
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    config = function()
+      require('catppuccin').setup {
+        flavour = 'mocha', -- You can set the flavor to 'mocha'
+      }
+      vim.cmd.colorscheme 'catppuccin' -- Set Catppuccin as the colorscheme
+    end,
+  },
+  {
+    'nvim-lualine/lualine.nvim',
+    opts = function()
+      require('lualine').setup {
+        options = {
+          theme = 'catppuccin', -- Use the Catppuccin theme for lualine
+          component_separators = '',
+          section_separators = { left = '', right = '' },
+        },
+        sections = {
+          lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
+          lualine_b = { 'filename', 'branch' },
+          lualine_c = {
+            '%=', --[[ add your center components here if needed ]]
+          },
+          lualine_x = {},
+          lualine_y = { 'filetype', 'progress' },
+          lualine_z = {
+            { 'location', separator = { right = '' }, left_padding = 2 },
+          },
+        },
+        inactive_sections = {
+          lualine_a = { 'filename' },
+          lualine_b = {},
+          lualine_c = {},
+          lualine_x = {},
+          lualine_y = {},
+          lualine_z = { 'location' },
+        },
+        tabline = {},
+        extensions = {},
+      }
+    end,
+  },
+}
