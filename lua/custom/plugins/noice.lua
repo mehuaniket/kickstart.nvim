5e5855ac7ca2fc6d75215f3691769e62edb91411
Aniket Patel <8078990+mehuaniket@users.noreply.github.com>
2024-09-25 09:46:58 +0530

    Added completion plugin for cmdline for noice

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -1,6 +1,5 @@
 return {
   {
-
     'folke/noice.nvim',
     event = 'VeryLazy',
     opts = {
@@ -13,6 +12,8 @@ return {
       --   `nvim-notify` is only needed, if you want to use the notification view.
       --   If not available, we use `mini` as the fallback
       'rcarriga/nvim-notify',
+      'hrsh7th/nvim-cmp',
+      'hrsh7th/cmp-cmdline',
     },
   },
 }
