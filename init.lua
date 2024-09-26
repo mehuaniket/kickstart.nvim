be1e0856a2b41e19ad738b46b0b85c16bed370fa
Aniket Patel <8078990+mehuaniket@users.noreply.github.com>
2024-09-26 10:51:23 +0530

    Added cmpline config to autosuggest on cmdline

--- init.lua
+++ init.lua
@@ -901,6 +901,29 @@ require('lazy').setup({
           { name = 'path' },
         },
       }
+
+      -- `/` cmdline setup.
+      cmp.setup.cmdline('/', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' },
+        },
+      })
+
+      -- `:` cmdline setup.
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' },
+        }, {
+          {
+            name = 'cmdline',
+            option = {
+              ignore_cmds = { 'Man', '!' },
+            },
+          },
+        }),
+      })
     end,
   },
 
