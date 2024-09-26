d8f8907cbc10032e70375e006bacf3e27875367e
Aniket Patel <8078990+mehuaniket@users.noreply.github.com>
2024-09-26 11:28:00 +0530

    removed lualine from neo-tree file type

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -17,14 +17,15 @@ return {
           theme = 'catppuccin', -- Use the Catppuccin theme for lualine
           component_separators = '',
           section_separators = { left = '', right = '' },
+          disabled_filetypes = { 'neo-tree', statusline = {}, winbar = {} },
         },
         sections = {
           lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
-          lualine_b = { 'filename', 'branch' },
+          lualine_b = { 'filename', 'branch', 'diff' },
           lualine_c = {
             '%=', --[[ add your center components here if needed ]]
           },
-          lualine_x = {},
+          lualine_x = { 'diagnostics', 'encoding' },
           lualine_y = { 'filetype', 'progress' },
           lualine_z = {
             { 'location', separator = { right = '' }, left_padding = 2 },
