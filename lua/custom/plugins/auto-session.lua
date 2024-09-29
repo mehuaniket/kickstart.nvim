6384c00765cee03227796183f15c5952584e54a5
Aniket Patel <8078990+mehuaniket@users.noreply.github.com>
2024-09-29 19:43:03 +0530

    Added auto-session plugin

---
+++ lua/custom/plugins/auto-session.lua
@@ -0,0 +1,18 @@
+return {
+  'rmagatti/auto-session',
+  lazy = false,
+
+  keys = {
+    -- Will use Telescope if installed or a vim.ui.select picker otherwise
+    { '<leader>wl', '<cmd>SessionSearch<CR>', desc = 'Session search' },
+    { '<leader>wx', '<cmd>SessionSave<CR>', desc = 'Save session' },
+    { '<leader>wa', '<cmd>SessionToggleAutoSave<CR>', desc = 'Toggle autosave' },
+  },
+  ---enables autocomplete for opts
+  ---@module "auto-session"
+  ---@type AutoSession.Config
+  opts = {
+    suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+    -- log_level = 'debug',
+  },
+}
