018b4bd660771fc285cd49b582ce121b5484e365
Aniket Patel <8078990+mehuaniket@users.noreply.github.com>
2025-01-28 20:38:17 +0530

    change copilot keymapping

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -7,7 +7,10 @@ return {
     event = 'InsertEnter',
     config = function()
       vim.g.copilot_no_tab_map = true
-      vim.api.nvim_set_keymap('i', '<C-J>', 'copilot#Accept("<CR>")', { silent = true, expr = true })
+      vim.g.copilot_filetypes = {
+        ['*'] = true,
+      }
+      vim.api.nvim_set_keymap('i', '<C-C>', 'copilot#Accept("<CR>")', { silent = true, expr = true })
     end,
   },
 }
